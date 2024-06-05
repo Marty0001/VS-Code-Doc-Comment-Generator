ed29a936c59a287de3ff4f03663129f3da69b453
Marty0001 <123718743+Marty0001@users.noreply.github.com>
2024-06-05 19:29:20 -0400

    Delete test/languages directory

--- test/languages/cpp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-std::vector<int> processArray(std::vector<int> arr, std::string word) {
-    for (int &item : arr) {
-        item = word.length();
-    }
-    return arr;
-}
